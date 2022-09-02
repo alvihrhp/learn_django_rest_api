621efaedf63d09c06b0d587047b6907299a78790
Rafael Harahap <rafaelharahap@Rafaels-MacBook-Pro.local>
2022-09-02 16:00:08 +0700

    1. Experiment with tailwind notification UI logic, 2. Install psycopg2 on django, 3. Dump current data and saved it as data.json, 4. Migrate from sqlite to postgre, 5. Load data from data.json and insert it to postgre

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,7 +17,7 @@ def createData(request):
     serializer = TodoSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+    return Response(serializer.data, 201)
 
 
 @api_view(['PUT'])
@@ -27,7 +27,7 @@ def updateData(request):
         id=primary_key, defaults=request.data)
     updatedTodo = Todo.objects.get(id=primary_key)
     serializer = TodoSerializer(updatedTodo)
-    return Response(serializer.data)
+    return Response(serializer.data, 200)
 
 
 @api_view(['DELETE'])
@@ -36,4 +36,4 @@ def deleteData(request):
     deletedTodo = Todo.objects.get(id=primary_key).delete()
     todos = Todo.objects.all()
     serializer = TodoSerializer(todos, many=True)
-    return Response(serializer.data)
+    return Response(serializer.dat, 200)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todo_rest_api_django',
+        'USER': 'rafaelharahap',
+        'PASSWORD': 'b1625nfb',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
